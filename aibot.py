db34b162e3c22153b1b2e83b42a95f9b3c3a2003
opegusikus <oleg.statkevych@gmail.com>
2026-02-13 22:55:17 +0900

    one session user distinction added

--- aibot.py
+++ aibot.py
@@ -11,47 +11,75 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 client = OpenAI(api_key=OPENAI_API_KEY, base_url="https://api.deepseek.com")
 
-user_instructions = {}
-user_state = {}
+users = {}
+
 final_instructions = ""
 system_instructions = f"""
-Listen all and obey all instructions here.
+Listen and obey all instructions here.
 You are useful AI agent.
-        """
+Maximum answer length is 2048 symbols.
+Next are user instructions if present:"""
+
+def remember_user(message):
+        #print('Im in remember user for {users[user_id][\'username\']}')
+        user_id = message.chat.id
+
+        if user_id not in users:
+                users[user_id] = {
+                        'username': None,
+                        'instructions': '',
+                        'user_state': None,
+                        'language': None,
+                        'tokens_today': None,
+                        'history': None
+                        }
+                users[user_id]['username'] = message.chat.username
+                users[user_id]['language'] = message.from_user.language_code
+        return users
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-        bot.reply_to(message, "This is custom AI agent based on Deepseek. \nYou can input your custom instructions to this agent /customInstruction")
+        remember_user(message)
+        
+        print(users)
+        bot.reply_to(message, "This is custom AI agent based on Deepseek. Just write message normaly AI will answer. \nYou can input your custom instructions to this agent /custominstruction")
 
-@bot.message_handler(commands=['customInstruction'])
+@bot.message_handler(commands=['custominstruction'])
 def user_settings(message):
+        remember_user(message)
         user_id = message.chat.id
-        user_state[user_id] = "waiting_for_instructions"
+        users[user_id]['user_state'] = "waiting_for_instructions"
         bot.reply_to(message, 'Now send your instructions to the AI agent')
 
-def ask_ai(prompt):
-        
-        
+def ask_ai(prompt):   
         response = client.chat.completions.create(
                 model="deepseek-chat",
+                max_tokens=1024,
+                temperature=0.3,
                 messages=[
                         {"role": "system", "content": final_instructions},
                         {"role": "user", "content": prompt}
                         ],
                 #stream=False
                 )
-        print(response)
+        #print(response)
+        if type(response.choices[0].message.content) == 'str':
+                print('\nAI answered')
         return response.choices[0].message.content
 
 @bot.message_handler(content_types=['text'])
 @bot.message_handler(func=lambda m: True)
 def handle_message(message):
         user_id = message.chat.id
-        if user_state.get(user_id) == "waiting_for_instructions":
-                user_instructions = message.text
-                user_state[user_id] = None
+        remember_user(message)
+        print(f'Id: {message.chat.id}, user: {message.chat.username}')
+        #print(message)
+        print(f'Im in message handler for {users[user_id][\'username\']}')
+        if users[user_id]['user_state'] == "waiting_for_instructions":
+                users[user_id]['instructions'] = message.text
+                users[user_id]['user_state'] = None
                 bot.reply_to(message, "Instructions applied!")
-                final_instructions = system_instructions + "\n" + user_instructions
+                final_instructions = system_instructions + "\n" + users[user_id]['instructions']
                 print(final_instructions)
                 return
         answer = ask_ai(message.text)
